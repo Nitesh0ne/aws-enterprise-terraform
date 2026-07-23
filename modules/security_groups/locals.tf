b66934d9996d199a0eb45d4a3122cf6f98267ca6
nitesh <pingnitesh07@gmail.com>
2026-07-23 11:27:03 +0545

    feat(network): implement reusable security groups module

--- modules/security_groups/locals.tf
+++ modules/security_groups/locals.tf
@@ -19,33 +19,102 @@ locals {
 
   }
 
-  
+  ingress_rules = {
 
-    alb_ingress_rules = [
-    {
-      description = "Allow HTTP"
-      from_port   = 80
-      to_port     = 80
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    },
-    {
-      description = "Allow HTTPS"
-      from_port   = 443
-      to_port     = 443
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  ]
+    alb = [
+      {
+        description   = "HTTP"
+        from_port     = 80
+        to_port       = 80
+        protocol      = "tcp"
+        cidr_ipv4     = "0.0.0.0/0"
+        source_sg_key = null
+      },
+      {
+        description   = "HTTPS"
+        from_port     = 443
+        to_port       = 443
+        protocol      = "tcp"
+        cidr_ipv4     = "0.0.0.0/0"
+        source_sg_key = null
+      }
+    ]
 
-  alb_egress_rules = [
-    {
-      description = "Allow all outbound traffic"
-      from_port   = 0
-      to_port     = 0
-      protocol    = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  ]
+    ec2 = [
+      {
+        description   = "HTTP from ALB"
+        from_port     = 80
+        to_port       = 80
+        protocol      = "tcp"
+        cidr_ipv4     = null
+        source_sg_key = "alb"
+      }
+    ]
+
+    rds = [
+      {
+        description   = "MySQL from EC2"
+        from_port     = 3306
+        to_port       = 3306
+        protocol      = "tcp"
+        cidr_ipv4     = null
+        source_sg_key = "ec2"
+      }
+    ]
+
+  }
+
+  egress_rules = {
+
+    alb = [
+      {
+        description   = "Allow all outbound"
+        from_port     = 0
+        to_port       = 0
+        protocol      = "-1"
+        cidr_ipv4     = "0.0.0.0/0"
+        source_sg_key = null
+      }
+    ]
+
+    ec2 = [
+      {
+        description   = "Allow all outbound"
+        from_port     = 0
+        to_port       = 0
+        protocol      = "-1"
+        cidr_ipv4     = "0.0.0.0/0"
+        source_sg_key = null
+      }
+    ]
+
+    rds = [
+      {
+        description   = "Allow all outbound"
+        from_port     = 0
+        to_port       = 0
+        protocol      = "-1"
+        cidr_ipv4     = "0.0.0.0/0"
+        source_sg_key = null
+      }
+    ]
+
+  }
+
+  flattened_ingress_rules = flatten([
+    for sg_name, rules in local.ingress_rules : [
+      for rule in rules : merge(rule, {
+        security_group = sg_name
+      })
+    ]
+  ])
+
+  flattened_egress_rules = flatten([
+    for sg_name, rules in local.egress_rules : [
+      for rule in rules : merge(rule, {
+        security_group = sg_name
+      })
+    ]
+  ])
 
 }
--- modules/security_groups/main.tf
+++ modules/security_groups/main.tf
@@ -4,11 +4,59 @@ resource "aws_security_group" "this" {
 
   name        = each.value.name
   description = each.value.description
-
-  vpc_id = var.vpc_id
+  vpc_id      = var.vpc_id
 
   tags = {
     Name = each.value.name
   }
+}
 
+###############################################################################
+# Ingress Rules
+###############################################################################
+
+resource "aws_vpc_security_group_ingress_rule" "this" {
+
+  for_each = {
+    for rule in local.flattened_ingress_rules :
+    "${rule.security_group}-${replace(lower(rule.description), " ", "-")}" => rule
+  }
+
+  security_group_id = aws_security_group.this[each.value.security_group].id
+
+  description = each.value.description
+
+  from_port   = each.value.from_port
+  to_port     = each.value.to_port
+  ip_protocol = each.value.protocol
+
+  cidr_ipv4 = each.value.cidr_ipv4
+
+  referenced_security_group_id = (
+    each.value.source_sg_key != null
+    ? aws_security_group.this[each.value.source_sg_key].id
+    : null
+  )
+}
+
+###############################################################################
+# Egress Rules
+###############################################################################
+
+resource "aws_vpc_security_group_egress_rule" "this" {
+
+  for_each = {
+    for rule in local.flattened_egress_rules :
+    "${rule.security_group}-${replace(lower(rule.description), " ", "-")}" => rule
+  }
+
+  security_group_id = aws_security_group.this[each.value.security_group].id
+
+  description = each.value.description
+
+  from_port   = each.value.from_port
+  to_port     = each.value.to_port
+  ip_protocol = each.value.protocol
+
+  cidr_ipv4 = each.value.cidr_ipv4
 }
